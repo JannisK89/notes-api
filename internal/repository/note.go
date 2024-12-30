9c47dbf9070f55f9dfa84b6002066196b2d6f627
JannisK89 <jannis.karanikis@gmail.com>
2024-12-30 13:16:10 +0100

    better repo error handling

--- internal/repository/note.go
+++ internal/repository/note.go
@@ -2,9 +2,24 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/JannisK89/notes-api/internal/models"
 )
+
+type RepoError struct {
+	Src string
+	ID  int
+	Err error
+}
+
+func (e *RepoError) Error() string {
+	return fmt.Sprintf("NoteRepo Error in %s with id %d: %v", e.Src, e.ID, e.Err)
+}
+
+func (e *RepoError) Unwrap() error {
+	return e.Err
+}
 
 type noteRepository struct {
 	db *sql.DB
@@ -14,24 +29,15 @@ func NewNotesRepository(db *sql.DB) *noteRepository {
 	return &noteRepository{db}
 }
 
-func (r *noteRepository) CreateNote(note *models.Note) (int, error) {
-	res, err := r.db.Exec("INSERT INTO notes (title, content) VALUES (?, ?)", note.Title, note.Content)
-	if err != nil {
-		return 0, err
-	}
-	id, err := res.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-	return int(id), nil
-}
-
 func (r *noteRepository) GetNoteByID(id int) (*models.Note, error) {
 	row := r.db.QueryRow("SELECT id, title, content FROM notes WHERE id = ?", id)
 	note := &models.Note{}
 	err := row.Scan(&note.ID, &note.Title, &note.Content)
 	if err != nil {
-		return nil, err
+		if err == sql.ErrNoRows {
+			return nil, &RepoError{"GetNoteByID", id, fmt.Errorf("Note not found", id)}
+		}
+		return nil, &RepoError{"GetNoteByID", id, err}
 	}
 	return note, nil
 }
@@ -39,7 +45,7 @@ func (r *noteRepository) GetNoteByID(id int) (*models.Note, error) {
 func (r *noteRepository) GetAllNotes() ([]*models.Note, error) {
 	rows, err := r.db.Query("SELECT id, title, content FROM notes")
 	if err != nil {
-		return nil, err
+		return nil, &RepoError{Src: "GetAllNotes", Err: err}
 	}
 	defer rows.Close()
 
@@ -48,17 +54,32 @@ func (r *noteRepository) GetAllNotes() ([]*models.Note, error) {
 		note := &models.Note{}
 		err := rows.Scan(&note.ID, &note.Title, &note.Content)
 		if err != nil {
-			return nil, err
+			if err == sql.ErrNoRows {
+				return nil, &RepoError{"GetAllNotes", 0, fmt.Errorf("No notes found")}
+			}
+			return nil, &RepoError{"GetAllNotes", 0, err}
 		}
 		notes = append(notes, note)
 	}
 	return notes, nil
 }
 
+func (r *noteRepository) CreateNote(note *models.Note) (int, error) {
+	res, err := r.db.Exec("INSERT INTO notes (title, content) VALUES (?, ?)", note.Title, note.Content)
+	if err != nil {
+		return 0, &RepoError{"CreateNote", 0, err}
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, &RepoError{"CreateNote", 0, err}
+	}
+	return int(id), nil
+}
+
 func (r *noteRepository) UpdateNoteByID(id int, note *models.Note) error {
 	_, err := r.db.Exec("UPDATE notes SET title = ?, content = ? WHERE id = ?", note.Title, note.Content, id)
 	if err != nil {
-		return err
+		return &RepoError{"UpdateNoteByID", id, err}
 	}
 	return nil
 }
@@ -66,7 +87,7 @@ func (r *noteRepository) UpdateNoteByID(id int, note *models.Note) error {
 func (r *noteRepository) DeleteNoteByID(id int) error {
 	_, err := r.db.Exec("DELETE FROM notes WHERE id = ?", id)
 	if err != nil {
-		return err
+		return &RepoError{"DeleteNoteByID", id, err}
 	}
 	return nil
 }
